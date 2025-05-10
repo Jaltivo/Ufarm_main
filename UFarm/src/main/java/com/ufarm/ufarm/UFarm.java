18f37f1eb4c72ff17e26796cfb42eb05eb206615
Jaltivo <jan.altivo@gmail.com>
2025-05-10 20:03:11 +0800

    First commit
    First commit

---
+++ UFarm/src/main/java/com/ufarm/ufarm/UFarm.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ufarm.ufarm;
+
+/**
+ *
+ * @author Admin
+ */
+public class UFarm {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
