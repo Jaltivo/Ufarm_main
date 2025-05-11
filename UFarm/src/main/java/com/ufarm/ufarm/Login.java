b1be0251f0a041cd7a7b3413f93a50b278d1465a
Jaltivo <jan.altivo@gmail.com>
2025-05-11 12:38:33 +0800

    Update Login.java

--- UFarm/src/main/java/com/ufarm/ufarm/Login.java
+++ UFarm/src/main/java/com/ufarm/ufarm/Login.java
@@ -85,7 +85,7 @@ public class Login extends javax.swing.JFrame {
         JOptionPane.showMessageDialog(this, "Invalid username or password", "Error", JOptionPane.ERROR_MESSAGE);
     }
 }  
-}  
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
