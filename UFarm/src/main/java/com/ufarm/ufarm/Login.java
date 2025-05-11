131b33f8710ce3bf6ddc76bedafce4f5fda72959
Jaltivo <jan.altivo@gmail.com>
2025-05-11 15:56:54 +0800

    Update Login.java

--- UFarm/src/main/java/com/ufarm/ufarm/Login.java
+++ UFarm/src/main/java/com/ufarm/ufarm/Login.java
@@ -389,7 +389,6 @@ public class Login extends javax.swing.JFrame {
             this.dispose();
         } else {
             JOptionPane.showMessageDialog(this, "Invalid email or password", "Error", JOptionPane.ERROR_MESSAGE);
-            this.dispose();
         }        
     }//GEN-LAST:event_LoginActionPerformed
 
